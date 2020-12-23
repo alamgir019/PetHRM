619f091959ac91f0c110b3af707a8ccbacb64f6b
Badsha, Alamgir EX1 <abadsha@us.imshealth.com>
2020-12-23 21:14:01 +0600

    addDbContext

---
+++ PetHRM.Repository/Data/PetHrmDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PetHRM.Repository.Data.Model;
+
+namespace PetHRM.Repository.Data
+{
+    public class PetHrmDbContext : DbContext
+    {
+        public PetHrmDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
